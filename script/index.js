317c0ac7041a264b7ca3dd636eaeaa14a52d5843
farcy <gilmanfarcy3@gmail.com>
2026-07-27 23:14:57 +0600

    more section added

---
+++ script/index.js
@@ -0,0 +1,27 @@
+const loadLessons = () => {
+    fetch("https://openapi.programming-hero.com/api/levels/all")//promise of response
+        .then((res) => res.json())//promise of data
+        .then((json) => displayLesson(json.data))//display data
+}
+const displayLesson = (lessons) => {
+    // get the container & empty
+    const levelContainer = document.getElementById("level-container");
+    levelContainer.innerHTML="";
+
+
+    // 2.get every lessons
+    for(let lesson of lessons){
+        const btnDiv = document.createElement("div");
+        btnDiv.innerHTML=`
+        
+        
+        `
+    }
+
+
+    // 3.create Element
+
+    // 4.append int container
+}
+
+loadLessons()
