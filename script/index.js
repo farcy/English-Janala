c0c4cdfb14c1f9da0f83d80942bbd88ee9e9c0b5
farcy <gilmanfarcy3@gmail.com>
2026-07-28 01:12:22 +0600

    level container added

--- script/index.js
+++ script/index.js
@@ -13,9 +13,11 @@ const displayLesson = (lessons) => {
     for(let lesson of lessons){
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML=`
-        
-        
-        `
+        <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
+        `;
+
+
+        levelContainer.append(btnDiv);
     }
 
 
